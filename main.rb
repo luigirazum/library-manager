5e4a1de6c7a0e7b4e9eed810cd7a02c6ea4fcd91
Luis Zubia <soyzubia@gmail.com>
2023-07-24 07:04:42 -0700

    Add test code for teacher class

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require_relative 'classes/person'
 require_relative 'classes/student'
+require_relative 'classes/teacher'
 
 puts '# create a person'
 puts '# person = Person.new(13, \'John\', parent_permission: false)'
@@ -15,3 +16,12 @@ student = Student.new(10, 'Kimberly', false, '1-A')
 p student
 puts '# output for calling \'play_hooky\' method'
 puts student.play_hooky
+
+puts "\n"
+puts '# create a teacher'
+puts '# teacher = Teacher.new(30, \'Charles\', \'Chemistry\')'
+puts '# the following is the \'teacher\' created'
+teacher = Teacher.new(30, 'Charles', 'Chemistry')
+p teacher
+puts '# output for calling \'use_services?\' method'
+puts teacher.use_services?
