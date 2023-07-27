525518379e037913bc90f2a77d6167d66f02b157
Luis Zubia <soyzubia@gmail.com>
2023-07-27 04:00:33 -0700

    Add base 'Decorator' class which receives a 'Nameable' object to initialize & Add 'correct_name' method which returns the result of 'correct_name' method from '@nameable' object

---
+++ classes/decorator.rb
@@ -0,0 +1,18 @@
+require_relative 'nameable'
+
+# The base 'Decorator' class follows the same interface as
+# the other components.
+class Decorator < Nameable
+  attr_accessor :nameable
+
+  # @param [Nameable] nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  # The 'Decorator' delegates all work to the wrapped component.
+  def correct_name
+    @nameable.correct_name
+  end
+end
