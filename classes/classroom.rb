cf3e72016a9fc210b474497c53ccaa7c66a7dd37
Luis Zubia <soyzubia@gmail.com>
2023-07-27 04:47:30 -0700

    Create the <kbd>Classroom</kbd> has many <kbd>Student</kbd>s relationship.

--- classes/classroom.rb
+++ classes/classroom.rb
@@ -1,7 +1,10 @@
 class Classroom
   attr_accessor :label
-
+  
+  # a 'Classroom' <has-many> 'Student's
+  # is set with @students = []
   def initialize(label)
     @label = label
+    @students = []
   end
 end
