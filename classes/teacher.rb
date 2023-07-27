c8b95f04089d07de059f027cb1c73a88320d1b7e
Luis Zubia <soyzubia@gmail.com>
2023-07-27 12:19:27 -0700

    Add 'att_reader' for '@specialization' so we can display it

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
+  attr_reader :specialization
+
   def initialize(age, name, specialization)
     super(age, name)
     @specialization = specialization
