45d0e0ff4e1c2177ced0d29252a01c6fc3a294f1
Luis Zubia <soyzubia@gmail.com>
2023-07-27 13:06:34 -0700

    Refactor 'classroom' setter in 'Student' so we are able to change the 'Classroom' for a 'Student' after initialization

--- classes/student.rb
+++ classes/student.rb
@@ -1,17 +1,20 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   # a 'Student' <belongs-to> a 'Classroom'
   # is set with @classroom = classroom
   # Make sure that when setting the Classroom for a Student
   # it also adds it to the Classrooms' Students
   # is set with @classroom.students.push(self) unless @classroom.students.include?(self)
-  def initialize(age, name, parent_permission, classroom)
+  def initialize(age, name, parent_permission)
     super(age, name, parent_permission: parent_permission)
+  end
+  
+  def classroom=(classroom)
     @classroom = classroom
-    @classroom.students << self unless @classroom.students.include?(self)
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
