a02ae438351e5a16756d9e2b9f4a80fc9b603d19
Luis Zubia <soyzubia@gmail.com>
2023-07-27 04:06:43 -0700

    Create decorator 'TrimmerDecorator' & Implement 'correct_name' method which returns the result of '@nameable.correct_name' trimming it to 10 characters max

---
+++ classes/trimmer_decorator.rb
@@ -0,0 +1,13 @@
+require_relative 'decorator'
+
+# Concrete Decorator 'TrimmerDecorator' call the wrapped object
+# and alter its result in some way.
+class TrimmerDecorator < Decorator
+  # Decorators may call parent implementation of the operation
+  # 'correct_name' trims the output to 10 chars maximum
+  def correct_name
+    return @nameable.correct_name[0, 10] if @nameable.correct_name.size > 10
+
+    @nameable.correct_name
+  end
+end
