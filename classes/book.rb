9ae8ad50406056ff3596b66478c2342118aa0b09
Luis Zubia <soyzubia@gmail.com>
2023-07-27 05:22:06 -0700

    Create the has-many side of 'Book' and 'Rental' (a 'Book' has many 'Rentals')

--- classes/book.rb
+++ classes/book.rb
@@ -1,8 +1,11 @@
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
+  # a 'Book' <has-many> 'Rental's
+  # is set with @rentals = []
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
   end
 end
