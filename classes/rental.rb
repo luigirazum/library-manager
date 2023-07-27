da976e62f7bb3d2e6cb878dd06591994beb1ae15
Luis Zubia <soyzubia@gmail.com>
2023-07-27 05:39:19 -0700

    Create the belongs-to side of 'Rental' and 'Person' (a 'Rental' belongs to a 'Person') & Make sure the 'Rental' is added to that 'Person'

--- classes/rental.rb
+++ classes/rental.rb
@@ -1,13 +1,17 @@
 class Rental
-  attr_reader :book
+  attr_reader :book, :person
   attr_accessor :date
 
-  # Add association
+  # Add associations
   #  - a 'Rental' <belongs-to> a 'Book'
   #    * is set with @book = book
-  def initialize(date, book)
+  #  - a 'Rental' <belongs-to> a 'Person'
+  #    * is set with @person = person
+  def initialize(date, book, person)
     @date = date
     @book = book
     @book.rentals << self unless @book.rentals.include?(self)
+    @person = person
+    @person.rentals << self unless @person.rentals.include?(self)
   end
 end
