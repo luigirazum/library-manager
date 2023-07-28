61686863cc6230d52c95d77f19d73a74fdfa70a1
Luis Zubia <soyzubia@gmail.com>
2023-07-28 11:09:04 -0700

    Refactor 'Menu' class to handle customized options

--- classes/ui/menu.rb
+++ classes/ui/menu.rb
@@ -1,16 +1,21 @@
 require_relative '../../modules/ui/menu_options'
+require_relative 'input'
+require_relative 'screen'
+require_relative 'error'
 
 class Menu
   include MenuOptions
 
-  def initialize(type)
+  def initialize(type, custom_options = [])
     @title = get_title(type)
-    @options = get_options(type)
+    @options = get_options(type, custom_options)
     @actions = get_actions(type)
     @selection = nil
   end
 
   def action
+    return @selection if @actions == :select
+
     @actions[@selection]
   end
 
@@ -20,18 +25,17 @@ class Menu
 
   def display
     title = "#{'-' * 20} #{@title} #{'-' * 20}"
-    puts title
+    puts title unless @title.is_a? Array
+
+    puts @title if @title.is_a? Array
+
     puts 'Below you\'ll find the available options'
-    puts '-' * title.length
+    puts '-' * 82
     @options.each_with_index do |option, i|
       puts "  #{i + 1} > #{option}"
     end
-    puts '-' * title.length
+    puts '-' * 82
     print 'Choose one option by entering its number: '
-  end
-
-  def input
-    gets.chomp.strip
   end
 
   def valid_option?
@@ -51,7 +55,30 @@ class Menu
   end
 
   def run
+    input = Input.new
     display
-    @selection = input
+    @selection = input.get
+  end
+
+  def select(title = nil)
+    @title = title unless title.nil?
+
+    screen = Screen.new
+    error = Error.new
+
+    screen.clear
+    loop do
+      screen.clear unless first_display?
+      error.set(nil)
+
+      run
+
+      break if exit?
+
+      return action if valid_option?
+
+      error.set('🚨 Please enter a valid option') unless valid_option?
+      error.display if error.exist?
+    end
   end
 end
